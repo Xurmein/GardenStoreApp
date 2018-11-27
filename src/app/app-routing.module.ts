92383c3d15cdec5373168df71a22c4e95ace1303
Gunjan Aulakh <gunjan@Gunjans-MacBook-Pro.local>
2018-11-27 16:46:37 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { AuthComponent } from './auth/auth.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'home', component: AppComponent },
   { path: 'aboutus', component: AboutUsComponent },
   { path: 'login', component: AuthComponent}
 
